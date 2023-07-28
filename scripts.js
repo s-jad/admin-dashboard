6b9ff010bdcf920670dabd260ddb7e984c94870b
s-jad <s_jad@web.de>
2023-07-28 16:14:10 +0200

    Feat: animated hamburger menu

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const sidebar = document.getElementById('sidebar');
+const mainGridLayout = document.getElementById('main-grid-layout');
+const toggleSidebarBtn = document.getElementById('minimize-sidebar-button');
+const toggleSidebar = document.getElementById('minimize-sidebar');
+const hamburgerMenu = document.getElementById('hamburger-menu');
+
+toggleSidebarBtn.addEventListener('click', function() {
+    sidebar.classList.toggle('invisible');
+
+    if (sidebar.classList.contains('invisible')) {
+        toggleSidebar.style.left = "0.5%";
+        toggleSidebarBtn.style.left = "0.5%";
+        toggleSidebar.style.filter = "invert(0%)";
+        hamburgerMenu.classList.toggle('collapsed');
+    } else {
+        toggleSidebar.style.left = "-5%";
+        toggleSidebarBtn.style.left = "-5%";
+        toggleSidebar.style.filter = "invert(100%)";
+        hamburgerMenu.classList.toggle('collapsed');
+    }
+});
+
