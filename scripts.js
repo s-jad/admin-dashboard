968f4ea91fd2277f493ddc06f2bea75fde09b00e
s-jad <s_jad@web.de>
2023-07-28 17:24:58 +0200

    Feat: collapsable searchbar

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@ const mainGridLayout = document.getElementById('main-grid-layout');
 const toggleSidebarBtn = document.getElementById('minimize-sidebar-button');
 const toggleSidebar = document.getElementById('minimize-sidebar');
 const hamburgerMenu = document.getElementById('hamburger-menu');
+const searchbar = document.getElementById('searchbar');
+const searchbarBtn = document.getElementById('searchbar-button');
 
 toggleSidebarBtn.addEventListener('click', function() {
     sidebar.classList.toggle('invisible');
@@ -12,13 +14,15 @@ toggleSidebarBtn.addEventListener('click', function() {
         toggleSidebarBtn.style.left = "0.5%";
         toggleSidebar.style.filter = "invert(0%)";
         hamburgerMenu.classList.toggle('collapsed');
-        mainGridLayout.style.gridTemplateColumns = "0fr 1fr";
     } else {
         toggleSidebar.style.left = "-5%";
         toggleSidebarBtn.style.left = "-5%";
         toggleSidebar.style.filter = "invert(100%)";
         hamburgerMenu.classList.toggle('collapsed');
-        mainGridLayout.style.gridTemplateColumns = "1fr 5fr";
     }
+});
+
+searchbarBtn.addEventListener('click', function() {
+    searchbar.classList.toggle('inactive');
 });
 
