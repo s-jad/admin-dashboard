8c579701b536b16ccb82165602b2ae8ffb42f924
s-jad <s_jad@web.de>
2023-07-28 16:35:40 +0200

    Feat: collapsable sidebar

--- scripts.js
+++ scripts.js
@@ -12,11 +12,13 @@ toggleSidebarBtn.addEventListener('click', function() {
         toggleSidebarBtn.style.left = "0.5%";
         toggleSidebar.style.filter = "invert(0%)";
         hamburgerMenu.classList.toggle('collapsed');
+        mainGridLayout.style.gridTemplateColumns = "0fr 1fr";
     } else {
         toggleSidebar.style.left = "-5%";
         toggleSidebarBtn.style.left = "-5%";
         toggleSidebar.style.filter = "invert(100%)";
         hamburgerMenu.classList.toggle('collapsed');
+        mainGridLayout.style.gridTemplateColumns = "1fr 5fr";
     }
 });
 
