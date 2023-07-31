f8b949b6a4c14cbc077cf1f9d61cefdd48c4c4f0
s-jad <s_jad@web.de>
2023-07-31 16:49:45 +0200

    Feats: better responsiveness, can delete projects

--- scripts.js
+++ scripts.js
@@ -14,9 +14,17 @@ const addProjectBtn = document.getElementById('add-project-button');
 const createProjectBtn = document.getElementById('create-new-project-button');
 const cancelProjectBtn = document.getElementById('cancel-new-project-button');
 const createProjectModal = document.getElementById('create-project-modal');
-const modalContent = document.getElementById('modal-content');
+const createProjectModalContent = document.getElementById('create-project-modal-content');
+
+const deleteProjectBtns = document.querySelectorAll('.delete-project-button');
+const deleteProjectModal = document.getElementById('delete-project-modal');
+const deleteProjectModalContent = document.getElementById('delete-project-modal-content');
+const deleteProjectModalTitle = document.getElementById('confirm-delete-project-title');
+const cancelDeleteProjectBtn = document.getElementById('cancel-delete-project-button');
+const confirmDeleteProjectBtn = document.getElementById('confirm-delete-project-button');
 
 // Toggle sidebar collapse/expansion and minus <-> hamburger animation
+// Only present when vertical scroll = 0
 toggleSidebarBtn.addEventListener('click', function() {
     sidebar.classList.toggle('invisible');
     if (sidebar.classList.contains('invisible')) {
@@ -24,14 +32,18 @@ toggleSidebarBtn.addEventListener('click', function() {
         toggleSidebarBtn.style.left = "0.5%";
         toggleSidebar.style.filter = "invert(0%)";
         hamburgerMenu.classList.toggle('collapsed');
+        mainGridLayout.classList.toggle('single-column');
     } else {
         toggleSidebar.style.left = "-5%";
         toggleSidebarBtn.style.left = "-5%";
         toggleSidebar.style.filter = "invert(100%)";
         hamburgerMenu.classList.toggle('collapsed');
+        mainGridLayout.classList.toggle('single-column');
     }
 });
 
+// Toggle sidebar collapse/expansion and minus <-> hamburger animation
+// Only present when user scrolls vertically
 scrollingToggleSidebarBtn.addEventListener('click', function() {
     sidebar.classList.toggle('invisible');
     // Ensure hamburger menu is visible to expand sidebar again
@@ -39,6 +51,7 @@ scrollingToggleSidebarBtn.addEventListener('click', function() {
     toggleSidebarBtn.style.left = "0.5%";
     toggleSidebar.style.filter = "invert(0%)";
     hamburgerMenu.classList.remove('collapsed');
+    mainGridLayout.classList.add('single-column');
 });
 
 // Collapse and Expand the searchbar
@@ -62,16 +75,21 @@ document.addEventListener('scroll', function() {
     }
 });
 
+// CREATE NEW PROJECTS
+
+// Opens the create new project modal
 addProjectBtn.addEventListener('click', function() {
     createProjectModal.classList.add('active');
-    modalContent.classList.remove('fade');
+    createProjectModalContent.classList.remove('fade');
 });
 
+// Closes the create new project modal
 cancelProjectBtn.addEventListener('click', function() {
     createProjectModal.classList.remove('active');
-    modalContent.classList.add('fade');
+    createProjectModalContent.classList.add('fade');
 });
 
+// Creates the new project and appends card to projects-inner-container
 createProjectBtn.addEventListener('click', function() {
     // Create new card node
     const card = document.createElement('div');
@@ -96,5 +114,38 @@ createProjectBtn.addEventListener('click', function() {
 
     // Close the modal
     createProjectModal.classList.remove('active');
-    modalContent.classList.add('fade');
+    createProjectModalContent.classList.add('fade');
+});
+
+// DELETE PROJECTS
+
+// Varies based on which delete project icon is clicked
+let projectToBeDeleted;
+let projectToBeDeletedTitle;
+
+// Opens delete project modal and sets projectToBeDeleted to clicked card
+deleteProjectBtns.forEach(function(btn) {
+    btn.addEventListener('click', function() {
+        deleteProjectModal.classList.add('active');
+        deleteProjectModalContent.classList.remove('fade');
+
+        // Assigns btn's grandparent div.card to projectToBeDeleted
+        projectToBeDeleted = btn.parentNode.parentNode;
+        projectToBeDeletedTitle = projectToBeDeleted.querySelector('.project-title');
+        deleteProjectModalTitle.innerText = `Are you sure you want to delete ${projectToBeDeletedTitle.innerText}?`;
+    });
+});
+
+// Closes delete project modal
+cancelDeleteProjectBtn.addEventListener('click', function() {
+    deleteProjectModal.classList.remove('active');
+    deleteProjectModalContent.classList.add('fade');
+});
+
+// Deletes project and removes from project inner container
+confirmDeleteProjectBtn.addEventListener('click', function() {
+    // Delete target project
+    projectGrid.removeChild(projectToBeDeleted);
+    deleteProjectModal.classList.remove('active');
+    deleteProjectModalContent.classList.add('fade');
 });
